e7235163f39c11b6c615419772021548a9a30373
luis-alvarez1 <44677628+luis-alvarez1@users.noreply.github.com>
2020-02-26 00:36:12 -0500

    primer programa

---
+++ Variables_tiposDatos_primProgram/primer_programa.py
@@ -0,0 +1,14 @@
+# primer programa! :D
+
+"""
+crear un programa que le pida al usuario dos números y muestre por pantalla la:
+    suma
+    resta
+    miltiplicación
+    y división
+de ambos número
+"""
+def main():
+    pass
+
+main()
