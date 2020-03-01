0cc1e78d3b41a9b4732a6784ba90c93603f23ab2
luis-alvarez1 <44677628+luis-alvarez1@users.noreply.github.com>
2020-03-01 00:51:50 -0500

    condiciones

--- Variables_tiposDatos_primProgram/primer_programa.py
+++ Variables_tiposDatos_primProgram/primer_programa.py
@@ -1,17 +1,20 @@
 # primer programa! :D
 
 """
-crear un programa que le pida al usuario dos números y muestre por pantalla la:
+crear un programa que le pida al usuario su edad, nombre y dos números y muestre por pantalla:
+
     suma
     resta
     miltiplicación
     y división
-de ambos número
+    de ambos número seguidos de su nombre y edad
+
+    NOTA: si el usuario no es mayor de 5 años, no puede usar el programa
 """
 
 
 def main():
-    pass
+    pass  # code here
 
 
 main()
