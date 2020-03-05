679de917c47211b1f6dc7b76b0fca27475d45065
luis-alvarez1 <44677628+luis-alvarez1@users.noreply.github.com>
2020-03-05 11:52:59 -0500

    dont care about this

--- Variables_tiposDatos_primProgram/2-operadores.py
+++ Variables_tiposDatos_primProgram/2-operadores.py
@@ -2,7 +2,7 @@
 
 """
 aritmeticos :
-    suma, resta, multiplicacio, division, exponencial, modulo, division entera
+    suma, resta, multiplicacio, division, exponencial, residuo, division entera
 
 comparacion (return bool):
     igual que, mayor que, menor que, diferente de, mayor o igual, menor o igual
@@ -19,7 +19,35 @@ especiales
 
 
 def main():
-    pass  # code here
+    # aritmeticos
+    num1 = 3
+    num2 = 87
+    '''
+    #suma
+    print(num1 + num2)
+    #resta
+    print(num1 - num2)
+    #multiplicacion
+    print(num1*num2)
+    #division
+    print(num1/num2)
+    #exponente
+    print(num1**num2)
+    #modulo/residuo
+    print(num1%num2)
+    #division entera
+    print(num1//num2)'''
+
+    '''
+    print(num1 == num2)
+    print(num1>num2)
+    print(num1<num2)
+    print(num1!=num2)
+    print(num1>=num2)
+    print(num1<= num2)
+    '''
+    num1 //= 3
+    print(num1)
 
 
 main()
