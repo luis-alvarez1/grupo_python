44d5d459e4b45e0df10dca8712951340c81058ce
Luis <Alvarez>
2020-03-29 09:57:42 -0500

    dont care about this

---
+++ Funciones/funciones_vacias.py
@@ -0,0 +1,24 @@
+# Funciones
+'''
+Una función es un conjunto de líneas de código que realizan una tarea específica y puede retornar un valor.
+
+Las funciones pueden tomar parámetros que modifiquen su funcionamiento.
+
+Las funciones son utilizadas para descomponer grandes problemas en tareas simples y para implementar operaciones que
+son comúnmente utilizadas durante un programa y de esta manera reducir la cantidad de código.
+
+sintaxis:
+
+def <nombre de la funcion> (<parametros>):
+    #cuerpo de la funcion
+
+clausula return: Las funciones pueden comunicarse con el exterior de las mismas, al proceso principal del programa
+usando la sentencia return. El proceso de comunicación con el exterior se hace devolviendo valores.
+'''
+
+
+def main():
+    pass  # code here
+
+
+main()
