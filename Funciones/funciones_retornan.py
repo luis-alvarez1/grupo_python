e6f162759815b66f839abd5c01bde90406f04176
Luis <Alvarez>
2020-04-02 00:15:42 -0500

    dont care about this

---
+++ Funciones/funciones_retornan.py
@@ -0,0 +1,19 @@
+'''
+las funciones que retornan, en otros lenguajes, se caracterizan por tener un tipo especificado en la definición.
+
+Esto no pasa en python. Aquí las funciones pueden retornar o no un valor, dependiendo de la situación y del contexto de
+la función.
+
+Las funciones que retornan están definidas por la clausula "return" en el cuerpo de la misma. Si se especifica al menos
+return dentro de la función, sí o sí, esta debe retornar algún valor.
+
+Estas funciones delvuelven un valor y, si no se almacena en una variable o estructura de datos, se perderá el valor. Por
+lo tanto, debemos asegurarnos de que el valor sea inyectado en otra variable.
+'''
+
+
+def main():
+    pass  # code here
+
+
+main()
