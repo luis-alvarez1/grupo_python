36e3d2ab93268833d2c16740e2051dea6715a5b6
Luis <Alvarez>
2020-04-30 11:51:56 -0500

    dont care about this

--- Bucles_EstDatos_segProgram/3-estDatos/ejercicios_array.py
+++ Bucles_EstDatos_segProgram/3-estDatos/ejercicios_array.py
@@ -5,9 +5,20 @@ Crea un array de 10 posiciones de números con valores pedidos por teclado.
 
 '''
 
+'''Crea un array de números de un tamaño pasado por teclado, 
+el array contendrá números aleatorios impares entre los números deseados, 
+por último nos indica cual es el mayor de todos.'''
+
+'''
+Crea un array de números de 100 posiciones, que contendrá los números del 
+1 al 100. Obtén la suma de todos ellos y el promedio. 
+
+    promedio = suma(elemntos)/elementos
+'''
+
 import random
 
-
+'''
 def rellenarVector(vector):
     for i in range(10):
         vector.append(random.randint(0, 30))
@@ -31,6 +42,33 @@ def main():
     rellenarVector(array)
 
     mostrarVector(array)
+'''
+
+
+def main():
+    size = int(input("Defina el tamaño del vector: "))  # 5
+
+    impares = []
+
+    while len(impares) < size:
+
+        for i in range(size):
+            num = random.randint(0, 100)
+
+            if len(impares) >= size:
+                break
+            elif num % 2 != 0:
+                impares.append(num)
+
+    # calculo mayor
+
+    mayor = 0
+    for i in range(0, size):
+        if impares[i] > mayor:
+            mayor = impares[i]
+
+    print(impares)
+    print("El mayor es " + str(mayor))
 
 
 main()
