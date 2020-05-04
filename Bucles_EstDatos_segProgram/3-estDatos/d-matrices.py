14c353fbd09c140ffeb873b4b8663406a8b4a03f
Luis <Alvarez>
2020-05-03 23:51:46 -0500

    dont care about this

---
+++ Bucles_EstDatos_segProgram/3-estDatos/d-matrices.py
@@ -0,0 +1,31 @@
+#Matrices
+
+'''
+Las matrices se pueden definir de dos formas:
+    Un vector bidimensional que ya no posee un solo indice sino que ahora posee subindices
+
+    Una estructura de control que almacena datos y accede a ellos mediante filas y coljumnas
+
+En este tipo de estructuras de control ya no tendremos solo una posicion del array, sino que tendremos una posicion
+definida por otras 2. En Python, estas se definen como listas dentro de listas... Las listas externas son las filas de
+la matriz y las listas internas son las columnas de la misma. Esto puede variar segun el criterio de cada persona.
+
+Supongamos que tenemos una matriz A
+
+Sintaxis:
+    A= [[a, b, c],[1, 2, 3],[true, false, 24.9]]
+
+    Aquí estamos definiendo que la matriz tendrá 3 fila y 3 columnas quedando así
+
+    a,     b,     c
+    1,     2,     3
+    True, False, 24.9
+
+'''
+
+
+def main():
+    pass  # code here
+
+
+main()
