9563c01213cde7846ca078e1f60b7ac1aedebabd
Luis Alvarez <lealvarezp@unac.edu.co>
2020-05-11 12:46:25 -0500

    dont care about this

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\admin\Desktop\Programación\FicherosPython\Grupo Python\grupo_python\venv
+set VIRTUAL_ENV=C:\Users\luisa\PycharmProjects\grupo_python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
