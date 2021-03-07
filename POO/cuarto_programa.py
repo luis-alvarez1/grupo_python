468bb43894f2592477d8a2fc82769b274667ee29
Luis Alvarez <lealvarezp@unac.edu.co>
2021-03-06 23:05:08 -0500

    agregando nuevos ejercicios para modulos

---
+++ POO/cuarto_programa.py
@@ -0,0 +1,11 @@
+"""
+Realizar un ejemplo de POO con 2 clases, cada clase con 3 atributos,
+getters/setters, toString y métodos de clase (aparte de los getter y setter)
+"""
+
+
+def main():
+    pass
+
+
+main()
